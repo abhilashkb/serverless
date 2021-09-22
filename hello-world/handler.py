711b04ef206838fedab8e89b36cb7136bf473eea
abhilashkb <abhilashkb9020@gmail.com>
2021-09-23 03:52:05 +0530

    first commit

---
+++ hello-world/handler.py
@@ -0,0 +1,16 @@
+import json
+
+
+def hello(event, context):
+   print("HI")
+
+    return "Hello-world"
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
